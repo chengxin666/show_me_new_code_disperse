9011aefbed03cf878dda33310e44b419fe1fb344
chengxin666 <34231215+chengxin666@users.noreply.github.com>
2018-04-09 11:30:26 +0800

    add a file about os module

---
+++ about_os.py
@@ -0,0 +1,20 @@
+import os
+
+abs_path = os.path.abspath(__file__)
+print('abs_path     :', abs_path)
+print('getcwd       :', os.getcwd())
+
+normal_path = os.path.normpath(abs_path)
+print('normal_path  :', normal_path)
+
+(x, y) = os.path.split(abs_path)
+print('split        :', x, '____', y)
+
+(x, y) = os.path.splitext(abs_path)
+print('splittext    :', x, '____',y)
+
+print('join         :', os.path.join(x, y))
+print('basename     :', os.path.basename(abs_path))
+print('dirname      :', os.path.dirname(abs_path))
+print('curdir       :', os.curdir())
+
